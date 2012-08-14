53d74e0273e4aae7a70832626b1701363d49ca97
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-08-14 19:59:46 -0400

    [MOD] Fixed errors
    This file is one i edited earlier without being able to run
    it properly. Now with these fixes we can get to having all the
    items (podcasts) listed, old and new.

--- feedScraper.py
+++ feedScraper.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Imports.
+from datetime import datetime
+import time
 # Local.
 import utils
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Turn the xml file into usable data. 
+# Turn the xml file into usable data.
 class FeedScraper:
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def __init__( self, setting, xmls ):
-        self.xmls = xmls 
+        self.xmls = xmls
         self.data = {}
-        for name, data in self.xmls.items():
+        for name, xml in self.xmls.iteritems():
             self.data[ name ] = []
-            for item in self.feedSanitise( setting, data ):
-                self[ name ].append( )
+            feedDict = setting.data[ "feeds" ][ name ]
+            for data in self.feedSanitise( setting, xml, feedDict ):
+                self.data[ name ].append( data )
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Turn the xml into an easy to use python object.
-    def feedSanitise( self, settings, xml ):
+    def feedSanitise( self, settings, xml, feedDict ):
         for itemNode in self.genItems( xml ):
             data = self.itemDict( itemNode )
             if self.gotAllData( data ):
@@ -26,22 +29,24 @@ class FeedScraper:
                 yield data
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Create the python dict for the podcast item 
+    # Create the python dict for the podcast item
     def itemDict( self, node ):
         data = {}
         for child in node.childNodes:
             if child.localName and child.firstChild:
                 if child.localName == "pubDate":
-                        time_format = "%a, %d %b %Y %H:%M:%S "
-                        dateText = child.firstChild.wholeText
-                        dateText = dateText[ : dateText.rfind("+") ]
-                        timeStamp = time.mktime(time.strptime(dateText, time_format))
-                        data[ "date" ] = datetime.datetime.fromtimestamp(timeStamp)
-                elif child.localName == "title":	
-                        data[ "title" ] = child.firstChild.wholeText.strip()
+                    timeFormat = "%a, %d %b %Y %H:%M:%S "
+                    dateText = child.firstChild.wholeText
+                    dateText = dateText[ : dateText.rfind( "+" ) ]
+                    timeStrp = time.strptime( dateText, timeFormat )
+                    timeStamp = time.mktime( timeStrp )
+                    data[ "date" ] = datetime.fromtimestamp( timeStamp )
+                elif child.localName == "title":
+                    data[ "title" ] = child.firstChild.wholeText.strip()
             elif child.localName:
                 if child.localName == "enclosure":
-                    data[ "url" ] = child.attributes[ "url" ].firstChild.wholeText.strip()
+                    urlChild = child.attributes[ "url" ].firstChild
+                    data[ "url" ] = urlChild.wholeText.strip()
         return data
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -53,38 +58,54 @@ class FeedScraper:
         return True
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Add in extra data with defaults if they're not set up for the feed. 
-    def addExtraData( self, data, feedDict):
+    # Add in extra data with defaults if they're not set up for the feed.
+    def addExtraData( self, data, feedDict ):
         data[ "nameFormat" ] = feedDict.get( "nameFormat", "( title, dateFormat )" )
         data[ "dateFormat" ] = feedDict.get( "dateFormat", "%Y-%m-%d_%H-%M-%S" )
         data[ "seperator" ] = feedDict.get( "seperator", "-" )
         data[ "space" ] = feedDict.get( "space", "_" )
         data[ "startDate" ] = feedDict.get( "startDate", None )
-        data[ "fileName" ] = getFileName( data )
+        data[ "fileName" ] = self.getFileName( data )
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Generator of all the nodes for 'items' e.g. each podcast. 
-    def genNodes( self, doc ):
+    # Generator of all the nodes for 'items' e.g. each podcast.
+    def genItems( self, doc ):
         return ( item
-            for rss in doc.childNodes 
-                if filterName( rss, "rss" )
+            for rss in doc.childNodes
+                if utils.xmlFilterName( rss, "rss" )
                     for chan in rss.childNodes
-                        if filterName( chan, "channel" )
+                        if utils.xmlFilterName( chan, "channel" )
                             for item in chan.childNodes
-                                if filterName( item, "item" ) 
+                                if utils.xmlFilterName( item, "item" )
         )
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Generator of all the nodes for 'items' e.g. each podcast.
+    def getFileName( self, data ):    
+        dateFormat = data[ "date" ].strftime( data[ "dateFormat" ] )
+        title = data[ "title" ].title()
+        nameFormat = eval( "(%s)" % ( data[ "nameFormat" ], ) )
+        seperator = data[ "seperator" ]
+        name =  '%s%s%s' % ( nameFormat[ 0 ], seperator, nameFormat[ 1 ] )
+        url = data[ "url" ]
+        fileExtension = url[ url.rfind("."):]
+        fileName = "%s%s" % ( name, fileExtension )
+        fileName = fileName.replace(" %s " % seperator, seperator )
+        fileName = fileName.replace( ", ", seperator ).replace( " ", "_")
+        return fileName
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Test run.
 if __name__ == "__main__":
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Imports needed for test. 
+    # Imports needed for test.
     from settingsJson import Settings
     from feedGrabber import FeedGrabber
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Set up the Settings and them use FeedGrabber.
     settings = Settings( "config.json" )
-    feedGraber = FeedGrabber( settings )
+    feedGrabber = FeedGrabber( settings )
     feedScraper = FeedScraper( settings, feedGrabber.xmls )
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Print out the data. 
+    # Print out the data.
     print feedScraper.data
