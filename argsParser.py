b2c87ae6b442fb42d2fae69bbc2d19776d7531fc
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-08-23 22:06:30 -0400

    [MOD] Whitespace

--- argsParser.py
+++ argsParser.py
@@ -10,13 +10,13 @@ import argparse
 # Argument parser class.
 class ArgsParser:
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Take the argv list from the command line. 
+    # Take the argv list from the command line.
     def __init__( self, argList ):
         self.parser = argparse.ArgumentParser( description='PyCast')
         self.parser.add_argument(
             '-u',
             "--update",
-            action = "store_true", 
+            action = "store_true",
             default = False,
             help = "Update the feeds by gettng the latest from the interent"
         )
@@ -47,19 +47,19 @@ class ArgsParser:
         self.generateData()
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # 
+    #
     def setArgs( self, argList ):
         self.argList = argList
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # 
+    #
     def generateData( self ):
         print "--------------------------------------"
         print self.argList
         print "--------------------------------------"
-        self.data = self.parser.parse_args( self.argList ) 
+        self.data = self.parser.parse_args( self.argList )
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # 
+    #
     def getData( self ):
         return self.data
 
